9be012357c1517dd3eefeadfb53532541f9802d7
Michael Lisitsa <michael.lisitsa@gmail.com>
2024-07-11 22:45:00 +1000

    Add navbar and remove nextjs starter

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import Navbar from "../components/Navbar";
+import Navbar from "../../components/Navbar";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <h1>Home Page</h1>
+      <h1>About Page</h1>
     </div>
   );
 }
---
+++ src/app/assets/Logo.tsx
@@ -0,0 +1,23 @@
+export default function Logo() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 120 30"
+      fill="none"
+      width={120}
+      height={30}
+      className="ml-2"
+    >
+      <text
+        x="0"
+        y="15"
+        fill="currentColor"
+        fontFamily="Verdana"
+        fontSize="14"
+        fontWeight="bold"
+      >
+        eco quest
+      </text>
+    </svg>
+  );
+}
